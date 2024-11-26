ba2cd808cc6e4b39f1c06938a5cf5b49774c5574
hongkai_q <hongkai_q@local>
2024-11-26 19:54:57 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven {
+            setUrl("https://qapm-maven.pkg.coding.net/repository/qapm_sdk/android_release/")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+
+        maven {
+            setUrl("https://maven.youzanyun.com/repository/maven-releases/")
+        }
+        maven {
+            setUrl("https://qapm-maven.pkg.coding.net/repository/qapm_sdk/android_release/")
+        }
+    }
+}
+
+rootProject.name = "example"
+include(":app")
+ 
