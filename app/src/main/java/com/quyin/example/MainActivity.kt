ece1662345be7c8ecc72908297556ae2d7e558aa
hongkai_q <hongkai_q@123>
2025-01-06 03:11:40 +0800

    Fix the issue that the webview input box pops up

--- app/src/main/java/com/quyin/example/MainActivity.kt
+++ app/src/main/java/com/quyin/example/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.quyin.example
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.webkit.WebSettings
 import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,18 +10,32 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
 
-//        AndroidBug5497Workaround.assistActivity(this)
-        val webView = findViewById<WebView>(R.id.webview)
-        webView.loadUrl("file:///android_asset/test.html")
-    }
+		enableEdgeToEdge()
+		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+			val rect = Rect()
+			v.getWindowVisibleDisplayFrame(rect)
+			v.layoutParams.height = rect.height() + systemBars.bottom + systemBars.top
+			v.requestLayout()
+			insets
+		}
+
+		val webView = findViewById<WebView>(R.id.webview)
+		webView.settings.apply {
+//			javaScriptEnabled = true
+			javaScriptCanOpenWindowsAutomatically = true
+			setSupportZoom(true)
+			cacheMode = WebSettings.LOAD_NO_CACHE
+			builtInZoomControls = true
+			domStorageEnabled = true
+			useWideViewPort = true
+			loadWithOverviewMode = true
+		}
+		webView.loadUrl("file:///android_asset/test.html")
+	}
 }
